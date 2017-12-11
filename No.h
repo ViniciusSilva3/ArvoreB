766b2d5ca9702b8bd10d45c0561130249ac076f0
Vinicius <vinicius@debian>
2017-12-11 12:38:55 -0200

    Adicionei acha ordem e modifiquei No.h

--- No.h
+++ No.h
@@ -1,11 +1,14 @@
 # define ordem 3
 
-typedef struct no {
+typedef struct no{
 
-	int n; //Numero de chaves que o nó possui
-	int chaves[ordem - 1]; // Um vetor que pode armazenar todas as chaves 
+	int n; //Numero de chaves que a pagina possui
+	int chaves[ordem - 1]; // Um vetor que pode armazenar todas as chaves
+  int prrchaves[ordem - 1];//um vetor que armazena as PRR das chaves no arquivo data.txt 
 	struct no *p[ordem]; // Numero de ponteiro que o nó possui
 
-}node
+}BTPAGE;
 
+/* Os vetores chaves e prrchaves estao relacionados da seguinte maneira, chave[x] = y,
+ prrchaves[x]= PRR(y). */
 
