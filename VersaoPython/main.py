70e063da1b6568d8a327535a173936060b3e5727
felipebayres <felipebayres@gmail.com>
2017-12-14 15:21:34 -0200

    Implementacao do menu do programa

--- VersaoPython/main.py
+++ VersaoPython/main.py
@@ -1,6 +1,5 @@
 import arvoreB
 import pecaquebrada
-
 tree = arvoreB.arvoreB(50)
 
 a = pecaquebrada.base("data.txt")
@@ -10,4 +9,34 @@ prr = a.getPRR()
 for i, j in zip(chaves, prr):
     tree.inserir([i, j])
 
-print tree
+print tree 
+while 1:
+	print 30 * "-" , "MENU" , 30 * "-"
+	print "[1]: Inserir elemento"
+	print "[2]: Remover elemento"
+	print "[3]: Buscar elemento"
+	print "[4]: Atualizar registro"
+	print "[5]: Sair do programa"
+	print 67 * "-"
+  	i = 1
+  	while i == 1:
+	  	try:
+	  		escolha = input("Entre com a opcao desejada: ")
+	  		i = 0
+	  	except:
+	  		print("Opcao inexistente!")
+
+  	if escolha == 1:
+  		break
+	elif escolha == 2:
+		break
+	elif escolha == 3:
+		break
+	elif escolha == 4:
+		break
+	elif escolha == 5:
+		break
+	else:
+		print("Opcao inexistente!")
+
+
