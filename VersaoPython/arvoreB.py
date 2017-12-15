8d3d4fbd907a6f8f631d4a71ccf97aadfbb43b77
augustofb2 <augustofb2@gmail.com>
2017-12-15 18:53:17 -0200

    Apagados métodos não utilizados

--- VersaoPython/arvoreB.py
+++ VersaoPython/arvoreB.py
@@ -5,36 +5,14 @@ class arvoreB(object):
         self.ocupMin = ordem // 2 - 1  # minimo de chaves no no
         self.ocupMax = ordem - 1  # maximo de chaves no no
 
-        self.clear()
+        self.raiz = arvoreB.No(self.ocupMax, True)
+        self.tamanho = 0
+
+        # self.clear()
 
     def __len__(self):
         return self.tamanho
 
-    def clear(self):
-        self.raiz = arvoreB.No(self.ocupMax, True)
-        self.tamanho = 0
-
-    def __contains__(self, x):
-        No = self.raiz
-        while True:
-            indice = No.busca(x)
-            if indice >= 0:
-                return True
-            elif No.is_folha():
-                return False
-            else:  # no interno
-                No = No.filhos[~indice]
-
-    def buscar(self, x):
-        No = self.raiz
-        while True:
-            indice = No.busca(x)
-            if indice >= 0:
-                return No.prr[indice]
-            elif No.is_folha():
-                return RuntimeError("Chave nao encontrada")
-            else:  # no interno
-                No = No.filhos[~indice]
 
     def inserir(self, x, prr):
         # primeiro eh tratada a divisao
@@ -76,12 +54,20 @@ class arvoreB(object):
                         filho = direita
                 No = filho
 
+    def buscar(self, x):
+        No = self.raiz
+        while True:
+            indice = No.busca(x)
+            if indice >= 0:
+                return No.prr[indice]
+            elif No.is_folha():
+                return RuntimeError("Chave nao encontrada")
+            else:  # no interno
+                No = No.filhos[~indice]
+
     def remove(self, x):
         if not self._remove(x):
             raise KeyError(str(x))
-
-    def discard(self, x):
-        self._remove(x)
 
     def _remove(self, x):
         # percorre arvore
@@ -129,44 +115,6 @@ class arvoreB(object):
                         raiz = self.raiz
                     No = filho
                     indice = No.busca(x)
-
-    # metodo para iterar pelas chaves de forma ordenada
-    def __iter__(self):
-        if self.tamanho == 0:
-            return
-
-        pilhaNo = []
-        indicePilha = []
-        No = self.raiz
-        while True:
-            pilhaNo.append(No)
-            indicePilha.append(0)
-            if No.is_folha():
-                break
-            No = No.filhos[0]
-
-        while len(pilhaNo) > 0:
-            No = pilhaNo.pop()
-            indice = indicePilha.pop()
-            if No.is_folha():
-                for x in No.chaves:
-                    yield x
-            else:
-                yield No.chaves[indice]
-                indice += 1
-                if indice < len(No.chaves):
-                    pilhaNo.append(No)
-                    indicePilha.append(indice)
-                No = No.filhos[indice]
-                while True:
-                    pilhaNo.append(No)
-                    indicePilha.append(0)
-                    if No.is_folha():
-                        break
-                    No = No.filhos[0]
-
-    def __str__(self):
-        return self.raiz.__str__()
 
     class No(object): # estrutura de um no
 
@@ -226,22 +174,6 @@ class arvoreB(object):
                 del self.filhos[filho_i]
             return self.remove_chave(chave_i)
 
-        # rotina para insercao em no cheio
-        def dividir(self):
-            if len(self.chaves) != self.ocupMax:
-                raise RuntimeError("No nao esta cheio")
-            ocupMin = self.ocupMax // 2
-            no_direita = arvoreB.No(self.ocupMax, self.is_folha())
-            c = self.chaves[ocupMin]
-            p = self.prr[ocupMin]
-            no_direita.chaves.extend(self.chaves[ocupMin + 1:])
-            no_direita.prr.extend(self.prr[ocupMin + 1:])
-            del self.chaves[ocupMin:]
-            del self.prr[ocupMin:]
-            if not self.is_folha():
-                no_direita.filhos.extend(self.filhos[ocupMin + 1:])
-                del self.filhos[ocupMin + 1:]
-            return no_direita, c, p
 
         # verifica se remocao requer tratamentos adicionais
         def verifica_remocao(self, indice):
@@ -289,8 +221,19 @@ class arvoreB(object):
             else:
                 raise AssertionError("Condicao inesperada")
 
-        def __str__(self, nivel = 0):
-                if self.is_folha():
-                    return "\tNo folha com {0} chaves no nivel {1}\n\tChaves: {2}\n".format(len(self.chaves), nivel, self.chaves)
-                else:
-                    return "No interno com com {0} chaves e {1} filhos no nivel {2}\n\tChaves: {3}\n\n".format(len(self.chaves), len(self.filhos), nivel, self.chaves)+'\n'.join([filho.__str__(nivel + 1) for filho in self.filhos])
+                # rotina para insercao em no cheio
+        def dividir(self):
+            if len(self.chaves) != self.ocupMax:
+                raise RuntimeError("No nao esta cheio")
+            ocupMin = self.ocupMax // 2
+            no_direita = arvoreB.No(self.ocupMax, self.is_folha())
+            c = self.chaves[ocupMin]
+            p = self.prr[ocupMin]
+            no_direita.chaves.extend(self.chaves[ocupMin + 1:])
+            no_direita.prr.extend(self.prr[ocupMin + 1:])
+            del self.chaves[ocupMin:]
+            del self.prr[ocupMin:]
+            if not self.is_folha():
+                no_direita.filhos.extend(self.filhos[ocupMin + 1:])
+                del self.filhos[ocupMin + 1:]
+            return no_direita, c, p
